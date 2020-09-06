717855dff1d964d13a1557ff8bdbb4cd8024d337
marble <marble@hamburg.ccc.de>
2020-09-06 20:09:03 +0200

    fixed resources import

--- nmigen_boards/colorlight.py
+++ nmigen_boards/colorlight.py
@@ -3,7 +3,7 @@ import subprocess
 
 from nmigen.build import *
 from nmigen.vendor.lattice_ecp5 import *
-from resources import *
+from .resources import *
 
 
 __all__ = ["Colorlight5A75BV7_0Platform"]
